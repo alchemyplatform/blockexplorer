91d62272988b4ad876f6d9e8689b877689daf634
theknightmare007 <knightmarecf@gmail.com>
2023-03-22 09:48:30 +0530

    inserted Table for block search and data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import getBalance from './components/getBalance';
+import getBlockbyNumber from './components/getBlockbyNumber';
 var Web3 = require('web3');
 
 
 function App() {
   const [gasPrice, setGasPrice] = useState('Loading');
   const[blockNumber , setBlockNumber] = useState('Loading')
+  
   const [address, setAddress] = useState('');
   const [balance, setBalance] = useState(':');
 
+  const[inputBlock , setBlockas] = useState();
+  const[blockResult, showBlockData] = useState('');
+
   const web3 = new Web3("https://eth-mainnet.g.alchemy.com/v2/TTLLqv5MxV8X-bMTZpWG70XuItqDGRTo");
+  const handleBlockBox = (event) => {
+    setBlockas(event.target.value);
+    showBlockData("please wait")
+  };
+
+  const handleBlockResult = async () => {
+    const blockData = await getBlockbyNumber(inputBlock);
+    showBlockData(JSON.parse(blockData));
+  };
 
   const handleAddressChange = (event) => {
     setAddress(event.target.value);
@@ -18,7 +32,6 @@ function App() {
   const handleClick = async () => {
     const balance = await getBalance(address);
     setBalance(balance);
-    
   }
   useEffect(() => {
     const intervalId = setInterval(async () => {
@@ -34,6 +47,7 @@ function App() {
 
 
   return (
+    <div>
     <div
       style={{
         background: "linear-gradient(to left, #903AB7 20%, #2A1B9A 80%)",
@@ -48,6 +62,7 @@ function App() {
       <p>
         Gas Price is: <strong>{gasPrice} Gwei</strong>
       </p>
+      <p>Block data for block 16856266 is:   </p>
       <p>
         Latest Mined Block Number is: <strong>{blockNumber}</strong>
       </p>
@@ -56,12 +71,54 @@ function App() {
         Make sure to click the grey colored button below to fetch balance <br/><br/>
         Goto <a href="etherscan.io">Etherscan.io</a> to pick any address and paste it below<br/>
         <input type="text" value={address} onChange={handleAddressChange} placeholder='Paste the Ethereum address here' />
-        
         <button onClick={handleClick}>Get balance</button> <br />
         
         The balance of the <br/>address {address} is :{balance} ETH
       </p>
     </div>
+    <div
+  style={{
+    background: "linear-gradient(to left, #903AB7 20%, #2A1B9A 80%)",
+    color: "white",
+    padding: "20px",
+    borderRadius: "10px",
+    borderTop: "5px solid cyan",
+     margin: 'auto'
+  }}
+>
+      <h1 align ="center">Block Info</h1><br/>
+
+      <input type="text" value={inputBlock} onChange={handleBlockBox} placeholder='Paste the Block Number here' />
+    <button onClick={handleBlockResult}>Click to know brief Block Info</button> <br />
+    <table style={{ border: '2px solid cyan', borderCollapse: 'collapse', width: '100%' }}>
+    <thead>
+      <tr>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Base Fee Per Gas</th>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Number of transactions in the block</th>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Gas Limit</th>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Gas Used</th>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Block Filled Percent</th>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Timestamp as per indian Timing</th>
+        <th style={{ border: '2px solid cyan', padding: '8px' }}>Total Difficulty</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td style={{ border: '2px solid cyan', padding: '8px' }}>{parseFloat(blockResult.baseFeePerGas).toFixed(8)}</td>
+        <td style={{ border: '2px solid cyan', padding: '8px', paddingLeft: '40px' }}>{blockResult.txnCount}</td>
+        <td style={{ border: '2px solid cyan', padding: '8px' }}>{blockResult.gasLimit}</td>
+        <td style={{ border: '2px solid cyan', padding: '8px' }}>{blockResult.gasUsed}</td>
+        <td style={{ border: '2px solid cyan', padding: '8px' }}>{blockResult.blockFilledPecent}</td>
+        <td style={{ border: '2px solid cyan', padding: '8px' }}>{new Date(blockResult.timestamp * 1000).toString()}</td>
+        <td style={{ border: '2px solid cyan', padding: '8px' }}>{blockResult.totalDifficulty}</td>
+      </tr>
+    </tbody>
+  </table>
+    </div>
+    </div>
+
+    
   )
+  
 }
 export default App;
--- src/components/GasPrice.js
+++ src/components/GasPrice.js
@@ -1,5 +1,7 @@
 //const address = "0x0Fd40853B3B8c7805158b862B76B35A2a27B596A";
 //const ContractAddress = "0x07a27472f5C3B6e494c865aDd34C4e1c8C14EB2e";
+
+
 //const contract = new web3.eth.Contract(abi.abi , ContractAddress)
 async function GetBalance() {
 
@@ -7,14 +9,9 @@ async function GetBalance() {
 const  url = "https://eth-mainnet.g.alchemy.com/v2/TTLLqv5MxV8X-bMTZpWG70XuItqDGRTo";
 const web3 = new Web3(url);
 const x = await web3.eth.getGasPrice();
- 
-   const Gas = web3.utils.fromWei(x,'gwei');
-    return(
-        <div>
-            <h1>current gas price is</h1>
-        </div>
-    )
+const gasin = web3.utils.fromWei(x , 'gwei')
+ console.log(gasin)
     
   }
-  
-export default GetBalance;
+  GetBalance();
+//export default GetBalance;

66a22926bcb5fee4004aa79d68951f221f8b625d
piyushjha0409 <peeyush0409@gmail.com>
2023-07-31 21:13:16 +0530

    getting block details

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
 // level code.
-const settings = {
+const config = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
@@ -17,20 +17,37 @@ const settings = {
 //
 // You can read more about the packages here:
 //   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-const alchemy = new Alchemy(settings);
+const alchemy = new Alchemy(config);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockDetails, setBlockDetails] = useState({});
+  const [nonce, setNonce] = useState();
+
+   //function for getting the blockNumber
+  async function getBlockNumber() {
+    setBlockNumber(await alchemy.core.getBlockNumber());
+  }
+
+  //function for getting the details of the block  
+ async function getBlockDetails(blockNumber){
+  let number = blockNumber.toString() 
+  setBlockDetails(await alchemy.core.getBlock(number))
+ }
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
-    }
-
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+
+  return (
+  <div>
+     <h1 className='heading'>Welcome to Scam ki duniya!!</h1>
+    <div className="App">Block Number: {blockNumber}</div>
+    <button onClick={()=> getBlockDetails({blockNumber})} className='fetch_button'> Get Details </button>
+    <div>Block's hash: {blockDetails}</div>
+  </div>
+  )
 }
 
 export default App;
